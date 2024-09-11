e65d13562cec1c45dd179926a46fedcf50275403
!Warwick7 <erick.dimer99@gmail.com>
2024-09-11 14:13:51 -0300

    Atualização de codigo

---
+++ 5_SECRET_WORD/secretword/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import StartScrenn from './components/StartScreen'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+          <StartScrenn />
+      </div>
+    </>
+  )
+}
+
+export default App
