c99b587eb549ed2d35c72fc1e1d72e9f4dff49ff
!Warwick7 <erick.dimer99@gmail.com>
2024-09-12 14:04:06 -0300

    Atualização de codigo

--- 5_SECRET_WORD/secretword/src/App.jsx
+++ 5_SECRET_WORD/secretword/src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
+import { useCallback, useEffect, useState } from "react";
 import StartScrenn from './components/StartScreen'
+import { wordsList } from "./data/words";
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
+
+const stages = [
+  {id:1, name:'start'}, 
+  {id:2, name:'game'}, 
+  {id:3, name:'end'}, 
+]
 
 function App() {
+
+  const [gameStage, setStage] = useState(stages[0].name)
 
   return (
     <>
       <div className='App'>
-          <StartScrenn />
+          {gameStage === 'start' && <StartScrenn/>}
+          {gameStage === 'game' && <Game/>}
+          {gameStage === 'end' && <GameOver/>}
       </div>
     </>
   )
---
+++ 5_SECRET_WORD/secretword/src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GameOver.css'
+
+const GameOver = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default GameOver
