758ade9e0f2a6a9761f7ddeba98a6e5214195d24
Érick Dimer <142959494+erickdimerr@users.noreply.github.com>
2025-04-01 23:06:57 -0300

    hooks

---
+++ 7_requsicaoHTTP/requisicao/src/hooks/useEffect.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+// 4 - Custom hook
+
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            
+
+        }
+    }, [url])
+}
